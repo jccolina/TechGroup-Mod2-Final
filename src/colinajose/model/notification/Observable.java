60465db9a762f781e00682952d5875f66aa7da07
Jose Colina <josseccb@gmail.com>
2020-07-04 23:58:51 -0400

    [Update] Models and class diagram Description =========== - Updated models and class diagram according to new approach

--- src/colinajose/model/notification/Observable.java
+++
@@ -1,8 +0,0 @@
-package colinajose.model.notification;
-
-public interface Observable {
-    void registerObserver(Observer observer);
-    void removeObserver(Observer observer);
-    void notifyObservers();
-    Object getState(Object object);
-}
--- src/colinajose/model/notification/Observer.java
+++
@@ -1,5 +0,0 @@
-package colinajose.model.notification;
-
-public interface Observer {
-    void update();
-}
